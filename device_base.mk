b0435678a1156ae5186137874e4012ea3e48fb74
Maciej Maj <maclaw@maclaw.pl>
2012-02-19 21:25:24 +0100

    Update to beta2: fixed browser, enable CPU sleep when screen is off, some small changes (more changes in config)

--- device_base.mk
+++ device_base.mk
@@ -204,10 +204,18 @@ $(call inherit-product, build/target/product/full_base.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += debug.sf.hw=1
 PRODUCT_PROPERTY_OVERRIDES += debug.composition.type=mdp
+PRODUCT_PROPERTY_OVERRIDES += debug.gr.numframebuffers=2
 
 # HardwareRenderer properties
 # dirty_regions: "false" to disable partial invalidates, override if enabletr=true
 PRODUCT_PROPERTY_OVERRIDES += \
     hwui.render_dirty_regions=false \
     hwui.disable_vsync=true \
-    hwui.print_config=choice
+    hwui.print_config=choice \
+    debug.enabletr=false
+
+# Misc properties
+# events_per_sec: default 90
+PRODUCT_PROPERTY_OVERRIDES += \
+    pm.sleep_mode=2 \
+    ro.telephony.call_ring.delay=2 \
